4c8f4ea62395fb2303fe39118b3b4776cbbb9124
“Devops <devopsproject8@gmail.com>
2023-11-30 01:58:29 +0530

    Deploy to GitHub Pages

---
+++ precache-manifest.add63c0fe45ddadb809c494583e02b9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6968ddec8176ce3e7b35e816a949a76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "947db3ae122181793ad5",
+    "url": "/static/js/2.106d0316.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.106d0316.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d854514e3d8acbed582",
+    "url": "/static/js/main.f18524ba.chunk.js"
+  },
+  {
+    "revision": "0cead240beaeac72778b",
+    "url": "/static/js/runtime-main.50b5716e.js"
+  },
+  {
+    "revision": "c11f97a3b321dad2ede69652d3f8a174",
+    "url": "/static/media/nattu-adnan-unsplash.c11f97a3.jpg"
+  }
+]);
